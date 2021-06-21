e32261eb4cf04e160a5120925c36bb6257605892
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-06-21 17:25:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img="";
+status="";
 
 function preload(){
     img=loadImage('dog_cat.jpg');
@@ -7,8 +8,15 @@ function preload(){
 function setup(){
     canvas=createCanvas(640,520);
     canvas.hide();
-}
 
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML='Status: object detecting';
+}
+function modelLoaded(){
+console.log('cocossd is initialized');
+status=true;
+objectDetector.detect(img,gotResult);
+}
 function draw(){
     image(img,0,0,640,520);
     fill('#FF0000');
@@ -16,5 +24,20 @@ function draw(){
     noFill();
     stroke('#FF0000');
     rect(30,60,450,350);
+
+    fill(' #0000FF ');
+    text('Cat',45,50);
+    noFill();
+    stroke(' #0000FF ');
+    rect(300,90,270,320);
     
+}
+
+function gotResult(error,results){
+if(results){
+console.log('results');
+}
+if(error){
+    console.log('error');
+}
 }
